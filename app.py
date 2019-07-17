037cba8f2a7e18d05e291f6735c97a7ddd6b9052
Simon Deeb <simon19-meet@meet.mit.edu>
2019-07-17 10:38:42 -0700

    fixed bugs

--- app.py
+++ app.py
@@ -49,6 +49,11 @@ def contact():
 	if request.method=='GET':
 		return render_template('contact.html')
 
+@app.route('/share', methods=['GET'])
+def share():
+	if request.method=='GET':
+		return render_template('share.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
