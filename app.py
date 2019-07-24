466dec15f259b37b0f39a08e84b5f4a2df5c0839
myname21-meet <myname21@meet.mit.edu>
2019-07-24 22:52:11 +0300

    heb

--- app.py
+++ app.py
@@ -26,16 +26,23 @@ def paypal():
 
 @app.route('/',methods=['GET','POST'])
 def home():
-	if request.method=='POST':
-		add_feedback(request.form['name'],request.form['age'],request.form['content'])
-		return redirect(url_for('home'))
 	if request.method=='GET':
-		return render_template('home.html', lang='he')
+		return render_template('home.html')
+
+@app.route('/he',methods=['GET','POST'])
+def home_he():
+	if request.method=='GET':
+		return render_template('home_he.html')
 
 @app.route('/about_us',methods=['GET'])
 def about_us():
 	if request.method=='GET':
 		return render_template('about_us.html')
+
+@app.route('/about_us_he',methods=['GET'])
+def about_us_he():
+	if request.method=='GET':
+		return render_template('about_us_he.html')
 
 @app.route('/volunteer_signup', methods=['GET', 'POST'])
 def volunteering():
@@ -52,20 +59,40 @@ def volunteering():
 	if request.method=='GET':
 		return render_template('volunteer.html')
 
+@app.route('/volunteer_signup_he', methods=['GET', 'POST'])
+def volunteering_he():
+	if request.method=='POST':
+		volunteer(request.form['name'], 
+			request.form['email'], 
+			request.form['gender'], 
+			request.form['age'], 
+			request.form['number'], 
+			request.form['address'], 
+			request.form['work'], 
+			request.form['academic_level'])
+		return redirect(url_for('thank_you_he'))
+	if request.method=='GET':
+		return render_template('volunteer_he.html')
+
 @app.route('/donations', methods=['GET', 'POST'])
 def donations():
 	if request.method=='GET':
 		return render_template('donations.html')
+
+@app.route('/donations_he', methods=['GET', 'POST'])
+def donations_he():
+	if request.method=='GET':
+		return render_template('donations_he.html')
 
 @app.route('/thank_you', methods=['GET'])
 def thank_you():
 	if request.method=='GET':
 		return render_template('thank_you.html')
 
-@app.route('/contact', methods=['GET'])
-def contact():
+@app.route('/thank_you_he', methods=['GET'])
+def thank_you_he():
 	if request.method=='GET':
-		return render_template('contact.html')
+		return render_template('thank_you_he.html')
 
 @app.route('/share', methods=['GET','POST'])
 def share():
@@ -94,6 +121,33 @@ def share():
 		return redirect(url_for('view',c=c))
 	return render_template('share.html')
 
+@app.route('/share_he', methods=['GET','POST'])
+def share_he():
+	c=len(session.query(Story).all())
+	if request.method=='POST':
+		print("hi")
+		print(request.form['name'])
+		print(request.form['age'])
+		print(request.form['content'])
+
+		name=request.form['name']
+		age=request.form['age']
+		content=request.form['content']
+		picBefore=request.files['before']
+		picAfter=request.files['after']
+		c+=1
+		strBef="static/images/before"
+		strBef+=str(c)
+		strAf="static/images/after"
+		strAf+=str(c)
+		picBefore.save(strBef)
+		picAfter.save(strAf)
+		story=Story(name=name,age=age,content=content,before=picBefore.read(),after=picAfter.read())
+		session.add(story)
+		session.commit()
+		return redirect(url_for('view_he',c=c))
+	return render_template('share_he.html')
+
 @app.route('/view_stories',methods=['GET'])
 def view():
 	print("hello viewer")
@@ -101,7 +155,12 @@ def view():
 		stories=display_stories()
 		return render_template('view_stories.html',stories=stories)
 
-
+@app.route('/view_stories_he',methods=['GET'])
+def view_he():
+	print("hello viewer")
+	if request.method=='GET':
+		stories=display_stories()
+		return render_template('view_stories_he.html',stories=stories)
 
 
 if __name__ == '__main__':
